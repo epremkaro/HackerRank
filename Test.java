b5a01256ded18caa68a4c05c18259dacf953dc29
exit <quit>
2017-09-05 17:58:38 +0300

    added isPolindromIdx

--- Test.java
+++ Test.java
@@ -1,7 +1,70 @@
+
 public class Test {
 
     public static void main (String[] args){
-        System.out.println("Hi");
+
+        String s = "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba" +
+                "abdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdbaabdffdba";
+
+        long start = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            isPolindromIdx(s);
+        }
+        System.out.println("Index \t" + (System.nanoTime() - start));
+
+        start = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            isPolindromSB(s);
+        }
+        System.out.println("SB \t" + (System.nanoTime() - start));
+
+//        System.out.println(isPolindromIdx(s));
+//        System.out.println(isPolindromSB(s));
     }
+
+    private static boolean isPolindromIdx(String s) {
+
+        boolean isPoli = true;
+        int tail = s.length()-1;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (i == tail || tail < i)
+                break;
+
+            if (s.charAt(i) == s.charAt(tail)) {
+                tail--;
+                continue;
+            }
+            else {
+                isPoli = false;
+                break;
+            }
+        }
+
+
+        return isPoli;
+    }
+
+
+    private static boolean isPolindromSB(String s){
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String rev = sb.toString();
+        if (rev.equals(s))
+            return true;
+
+        return false;
+    }
+
 
 }
