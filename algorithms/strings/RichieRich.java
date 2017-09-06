9cacedb107ae2a96166c5339eb8098e920f8e279
exit <quit>
2017-09-06 12:34:37 +0300

    added replace all with 9 when k >= n

--- algorithms/strings/RichieRich.java
+++ algorithms/strings/RichieRich.java
@@ -14,20 +14,30 @@ public class RichieRich {
 
         String[] sArr = s.split("");
 
-        if (isPalindrom(sArr) && k == 0)
-            return s;
+        if (isPalindrom(sArr)){
+            if (k == 0)
+                return s;
+
+            if (k >= n){
+                StringBuilder sb = new StringBuilder();
+                for(int max = 0; max < n; max++){
+                    sb.append("9");
+                }
+                return sb.toString();
+            }
+
+        }
 
         int tail = s.length() - 1;
         for (int i = 0; i < n && k > 0; i++, tail--) {
-
             if (!sArr[i].equals(sArr[tail])) {
-
                 if (k > 1){
                     sArr[i] = "9";
                     sArr[tail] = "9";
                     k -= 2;
                 }
-                else {
+
+                if (k == 1){
                     int a1 = Integer.parseInt(sArr[i]);
                     int a2 = Integer.parseInt(sArr[tail]);
                     if (a1 > a2)
@@ -37,67 +47,44 @@ public class RichieRich {
 
                     k--;
                 }
-                //System.out.print(Arrays.toString(sArr).replaceAll("\\[|\\]|,|\\s", ""));
             }
-
-
-            if (isPalindrom(sArr)) {
-                if (k == 1 && (n % 2) == 1)
-                    sArr[n/2] = "9";
-
-                while (k > 1) {
-                    //find min value of the palindromial string to replace it with '9'
-                    int min = 9;
-                    for (int j = 0; j < n; j++) {
-                        int jMin = Integer.parseInt(sArr[j]);
-                        if (jMin < min)
-                            min = jMin;
-                    }
-
-                    for (int m = 0; m < n && k > 1; m++) {
-                        if (sArr[m].equals(String.valueOf(min))){
-                            sArr[m] = "9";
-                            sArr[n - m - 1] = "9";
-                            k -= 2;
-                        }
-                    }
-                }
-
-
-                return Arrays.toString(sArr).replaceAll("\\[|\\]|,|\\s", "");
-            }
-
-            if (i == tail && (k % 2) == 1) {
-                sArr[i] = "9";
-                k--;
-                while (k > 0) {
-                    int min = 9;
-                    for (int j = 0; j < n; j++) {
-                        int jMin = Integer.parseInt(sArr[j]);
-                        if (jMin < min)
-                            min = jMin;
-                    }
-
-                    for (int m = 0; m < n; m++) {
-
-                    }
-                }
-
-            }
-
-            //23
-            if (i > tail) {
-                if (k > 1) {
-                    sArr[i] = "9";
-                    sArr[tail] = "9";
-                    k--;
-                    k--;
-                } else
-                    return "-1";
-            }
-
         }
-        return "-1";
+
+
+        while (k > 1) {
+            for (int m = 0; m < n && k > 1; m++) {
+                int a = Integer.parseInt(sArr[m]);
+                if (a < 9){
+                    sArr[m] = "9";
+                    sArr[n - m - 1] = "9";
+                    k -= 2;
+                }
+            }
+        }
+
+
+
+
+        while (k > 0) {
+            int min = 9;
+            for (int j = 0; j < n; j++) {
+                int jMin = Integer.parseInt(sArr[j]);
+                if (jMin < min)
+                    min = jMin;
+            }
+            for (int m = 0; m < n; m++) {
+
+            }
+        }
+
+        if (k == 1 && (n % 2) == 1)
+            sArr[n/2] = "9";
+
+
+        if (isPalindrom(sArr))
+            return Arrays.toString(sArr).replaceAll("\\[|\\]|,|\\s", "");
+        else
+            return "-1";
     }
 
     private static boolean isPalindrom(String[] arr) {
