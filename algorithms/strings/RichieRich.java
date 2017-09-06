4c34ee0a654f42f37cb7255e984c65dc5697dbc5
exit <quit>
2017-09-06 15:29:51 +0300

    RichieRich done

--- algorithms/strings/RichieRich.java
+++ algorithms/strings/RichieRich.java
@@ -8,88 +8,75 @@ import java.math.*;
 import java.util.regex.*;
 
 public class RichieRich {
-    static String richieRich(String s, int n, int k) {
-        if (s == null) return "-1";
-        if (s.length() == 1) return (k > 0) ? "9" : s;
+    static String richieRich(String str, int n, int k){
+        // Complete this function
 
-        if (k == 0){
-            if (isPalindrom(s))
-                return s;
-            else
-                return "-1";
-        }
 
-        if (k >= n){
-            StringBuilder res = new StringBuilder();
-            for (int max = 0; max < n; max++){
-                res.append("9");
+        char[] arr = str.toCharArray();
+
+        int x1 = 0;
+        int x2 = n-1;
+
+        while(x2>x1){
+            int num1 = (int)str.charAt(x1) - (int)'0';
+            int num2 = (int)str.charAt(x2) - (int)'0';
+
+            if(num1 == num2){
+                x1++; x2--;
             }
-            return res.toString();
-        }
-
-        String[] sArr = s.split("");
-        int tail = s.length() - 1;
-        for (int i = 0; i < n && k > 0; i++, tail--) {
-            if (!sArr[i].equals(sArr[tail])) {
-                if (k > 1){
-                    sArr[i] = "9";
-                    sArr[tail] = "9";
-                    k -= 2;
-                }
-
-                if (k == 1){
-                    int a1 = Integer.parseInt(sArr[i]);
-                    int a2 = Integer.parseInt(sArr[tail]);
-                    if (a1 > a2)
-                        sArr[tail] = sArr[i];
-                    else
-                        sArr[i] = sArr[tail];
-
-                    k--;
-                }
+            else if(num1 > num2){
+                arr[x2] = arr[x1];
+                x1++; x2--;
+                k--;
+            }
+            else{
+                arr[x1] = arr[x2];
+                x1++; x2--;
+                k--;
             }
         }
 
 
-        while (k > 1) {
-            for (int m = 0; m < n && k > 1; m++) {
-                int a = Integer.parseInt(sArr[m]);
-                if (a < 9){
-                    sArr[m] = "9";
-                    sArr[n - m - 1] = "9";
-                    k -= 2;
-                }
-            }
-        }
-
-        if (k == 1 && (n % 2) == 1)
-            sArr[n/2] = "9";
-
-
-        if (isPalindrom(sArr))
-            return Arrays.toString(sArr).replaceAll("\\[|\\]|,|\\s", "");
-        else
+        if(k < 0)
             return "-1";
-    }
+        else if(k == 0)
+            return new String(arr);
+        else{
+            x1 = 0;
+            x2 = n-1;
+            while(x2 > x1 && k > 0){
+                if(str.charAt(x1) == str.charAt(x2)){
+                    if(k > 1 && str.charAt(x1) != '9'){
+                        arr[x1] = '9';
+                        arr[x2] = '9';
+                        x1++; x2--;
+                        k -= 2;
+                    }
+                    else{
+                        x1++; x2--;
+                    }
+                }
+                else{
+                    if(arr[x1] != '9'){
+                        arr[x1] = '9';
+                        arr[x2] = '9';
+                        x1++; x2--;
+                        k--;
+                    }else{
+                        x1++; x2--;
+                    }
+                }
+            }
 
-    private static boolean isPalindrom(String str) {
-        return isPalindrom(str.split(""));
-    }
-
-    private static boolean isPalindrom(String[] arr) {
-        int tail = arr.length - 1;
-        for (int i = 0; i < arr.length; i++) {
-            if (i == tail || i > tail)
-                return true;
-
-            if (arr[i].equals(arr[tail])) {
-                tail--;
-                continue;
-            } else
-                return false;
+            if(k > 0){
+                if(n%2 != 0){
+                    arr[n/2] = '9';
+                }
+            }
+            return new String(arr);
         }
-        return false;
     }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
