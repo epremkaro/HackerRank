86a39f000758b798344bc6857b46ba7fd0dcc4f8
exit <quit>
2017-09-07 13:24:50 +0300

    RunningTimeOfAlgorithms done

--- algorithms/sorting/RunningTimeOfAlgorithms.java
+++ algorithms/sorting/RunningTimeOfAlgorithms.java
@@ -1,33 +1,42 @@
 package algorithms.sorting;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class RunningTimeOfAlgorithms {
 
-    public static int [] arr = {1, 2, 3, 5, 66, 33, 34, 31, 32};
-
     public static void main(String[] args) {
-
-        System.out.println("UnSorted :" + Arrays.toString(arr));
-        insertionSort(arr);
-        System.out.println("Sorted   :" + Arrays.toString(arr));
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int[] arr = new int[N];
+        for (
+                int i = 0;
+                i < N; i++)
+        {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(
+        getInsertSortShifts(arr));
     }
 
-    private static void insertionSort(int[] arr) {
+    private static int getInsertSortShifts(int[] arr) {
+        int shifts = 0;
         for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
+            int t = arr[i];
             int idx = i - 1;
-            while (idx > -1 && temp < arr[idx]){
-                swap(idx+1, idx);
+            while (idx > -1 && t < arr[idx]) {
+                swap(arr, idx + 1, idx);
+                shifts++;
                 idx--;
             }
         }
+        return shifts;
     }
 
-    private static void swap(int i1, int i2) {
-        int t = arr[i1];
+    private static void swap(int[] arr, int i1, int i2) {
+        int temp = arr[i1];
         arr[i1] = arr[i2];
-        arr[i2] = t;
+        arr[i2] = temp;
     }
 
 
